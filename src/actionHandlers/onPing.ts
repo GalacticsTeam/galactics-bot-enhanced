f077dc13f4988639b16c1a8ace8ec64055ab90db
omarlawaty <omarlawatey@gmail.com>
2024-05-25 19:01:31 +0300

    feat: Structure botConfig file

---
+++ src/actionHandlers/onPing.ts
@@ -0,0 +1,8 @@
+import { Message } from 'discord.js';
+import { botConfig } from '../utils';
+
+export const onPing = (msg: Message) => {
+  if (!botConfig.allowedFeatures.ping) return;
+
+  msg.reply({ content: 'Pong!' });
+};
