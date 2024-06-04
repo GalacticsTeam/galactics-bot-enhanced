bde996f2ab4692441161bddd2480bef766fa6196
omarlawaty <omarlawatey@gmail.com>
2024-06-04 04:10:09 +0300

    refact: Separate avatar placement into a function

--- src/actionHandlers/onWelcome.ts
+++ src/actionHandlers/onWelcome.ts
@@ -1,4 +1,4 @@
-import { GuildMember, TextChannel, channelMention, userMention } from 'discord.js';
+import { ChannelType, GuildMember, TextChannel, channelMention, userMention } from 'discord.js';
 import Jimp from 'jimp';
 
 const roles = {
@@ -14,16 +14,11 @@ const channels = {
 export const onWelcome = async (member: GuildMember) => {
   if (member.user.bot) return member.roles.add(roles.bot);
 
-  const welcomeChannel = member.guild.channels.cache.get(channels.welcomeChannel) as TextChannel;
+  const welcomeChannel = member.guild.channels.cache.get(channels.welcomeChannel);
+  if (welcomeChannel.type !== ChannelType.GuildText) return;
 
   const welcomeImage = (await Jimp.read('src/assets/welcomeImage.png')).scale(0.2);
-
-  const userImage = await Jimp.read(member.user.displayAvatarURL({ extension: 'png' }));
-  userImage.resize(122, 122);
-  userImage.circle();
-
-  welcomeImage.composite(userImage, 222, 36);
-
+  await addUserAvatar(welcomeImage, member);
   const welcomeImageBuffer = await welcomeImage.getBufferAsync('image/png');
 
   member.roles.add(roles.member).then(() => {
@@ -36,4 +31,11 @@ export const onWelcome = async (member: GuildMember) => {
         `);
     });
   });
+};
+
+const addUserAvatar = async (welcomeImage: Jimp, member: GuildMember) => {
+  const userImage = await Jimp.read(member.user.displayAvatarURL({ size: 128, extension: 'png' }));
+  userImage.resize(122, 122);
+  userImage.circle();
+  welcomeImage.composite(userImage, 222, 36);
 };
