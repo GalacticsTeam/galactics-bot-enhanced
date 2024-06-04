b4f9712986e02690a92c9e32aa5d62ad3bc2d166
nour-elhabbal <nour.elhabbal9@gmail.com>
2024-06-04 17:15:37 +0300

    feat: implement custom status

--- src/actions/onReady.ts
+++ src/actions/onReady.ts
@@ -2,10 +2,14 @@ import { Client } from 'discord.js';
 
 import { botConfig, isDevMode } from '../utils';
 import { commandsCreate } from '../actionHandlers';
+import { onCutsomStatus } from '../actionHandlers/onCustomStatus';
 
 export const onReady = <T extends boolean>(client: Client<T>) => {
   const guild = client.guilds.cache.get(botConfig.serverId);
   const commands = guild.commands;
+
+  // Custom status
+  onCutsomStatus(client);
 
   // Command Creation
   commandsCreate(commands);
