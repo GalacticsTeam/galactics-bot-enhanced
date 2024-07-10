5d066eec873367ebffd9eb26b7702873d98bf840
nour-elhabbal <nour.elhabbal9@gmail.com>
2024-07-10 22:14:55 +0300

    feat: warning system implementation

--- src/actionHandlers/commands/index.ts
+++ src/actionHandlers/commands/index.ts
@@ -14,6 +14,7 @@ import { unlockChannel } from './unlockChannel';
 import { lockChannel } from './lockChannel';
 import { serverConfig } from './serverConfig';
 import { modHelp } from './modHelp';
+import { warn } from './warn';
 
 export const commands = [
   { name: 'roll-dice', type: 'diceRoll', interaction: diceRoll },
@@ -26,6 +27,7 @@ export const commands = [
   { name: 'lock-channel', type: 'lockChannel', interaction: lockChannel },
   { name: 'server-config', type: 'serverConfig', interaction: serverConfig },
   { name: 'mod-help', type: 'modHelp', interaction: modHelp },
+  { name: 'warn', type: 'warn', interaction: warn },
 ] as const;
 
 export const commandsHandler = (interaction: ChatInputCommandInteraction) => {
