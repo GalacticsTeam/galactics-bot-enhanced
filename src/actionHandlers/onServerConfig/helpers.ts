e5f321606209140d82c1d05ed75ad2fabc498f74
OmarLawaty <omarlawatey@gmail.com>
2024-07-18 20:03:08 +0300

    fix: Add default server config to get the right config

--- src/actionHandlers/onServerConfig/helpers.ts
+++ src/actionHandlers/onServerConfig/helpers.ts
@@ -1,4 +1,7 @@
+import { DefaultServerConfig } from '../../types';
+import { defaultServerConfig } from '../../utils';
+
 import type { ServerConfig, ServerConfigOption } from './types';
 
 export const getUpdatedItem = <T extends ServerConfigOption>(itemType: T, filterFn: (item: string) => boolean) =>
-  Object.keys(itemType).find(filterFn) as ServerConfig<T>;
+  Object.keys(defaultServerConfig[itemType as keyof DefaultServerConfig]).find(filterFn) as ServerConfig<T>;
