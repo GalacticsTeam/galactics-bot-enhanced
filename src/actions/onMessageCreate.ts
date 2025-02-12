9c13fd1251ece6f961359f686df03832020087e1
OmarLawaty <omarlawatey@gmail.com>
2025-02-12 21:43:46 +0200

    chore: Remove forgotten debugging function

--- src/actions/onMessageCreate.ts
+++ src/actions/onMessageCreate.ts
@@ -1,7 +1,6 @@
 import type { Message } from 'discord.js';
 
 import { onPing, onLinkSend } from '@actionHandlers';
-import { onTranslate } from '@i18n/onTranslate';
 
 export const onMessageCreate = async (msg: Message<true>) => {
   if (msg.author.bot) return;
@@ -9,15 +8,6 @@ export const onMessageCreate = async (msg: Message<true>) => {
   // const command = msg.content.split(' ');
   // const commandName = command[0].toLowerCase();
   // const args = command.slice(1);
-  const t = onTranslate('en');
-
-  msg.reply(
-    t('welcome.message', {
-      user: '<@' + msg.author.id + '>',
-      rolesChannel: '<#' + msg.guild?.roles.cache.first()?.id + '>',
-      membersCount: msg.guild?.memberCount,
-    })
-  );
 
   onPing(msg);
 
